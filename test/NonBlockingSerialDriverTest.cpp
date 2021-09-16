b3f7154b6e47c90fced4f2ffa1432f0b8d7c1043
Joshua Moyerman <j.moyerman@gmail.com>
2021-09-16 12:22:57 -0400

    test ack, test two loops

--- test/NonBlockingSerialDriverTest.cpp
+++ test/NonBlockingSerialDriverTest.cpp
@@ -5,8 +5,15 @@
 
 using namespace fakeit;
 
+String lastMessage;
+
 String throwErrorCallback(String input) {
-    throw 1;
+    throw input;
+}
+
+String recordLastMessageCallback(String input) {
+    lastMessage = input;
+    return "";
 }
 
 void test_NoDataAvailable() {
@@ -21,10 +28,63 @@ void test_NoDataAvailable() {
     // No exception = pass
 }
 
+void test_SimpleMessage_OneIteration() {
+    // Setup
+    Serial_* serial = ArduinoFakeMock(Serial);
+    NonBlockingSerialDriver *underTest = new NonBlockingSerialDriver(serial, recordLastMessageCallback);
+    When(Method(ArduinoFake(Serial), available)).Return(1, 1, 0);
+    When(Method(ArduinoFake(Serial), read)).Return(':', '#');
+
+    // Execute
+    underTest->loop();
+
+    // Asserts
+    TEST_ASSERT_EQUAL_STRING(":#", lastMessage.c_str());
+}
+
+void test_SimpleMessage_TwoIterations() {
+    // Setup
+    Serial_* serial = ArduinoFakeMock(Serial);
+    NonBlockingSerialDriver *underTest = new NonBlockingSerialDriver(serial, recordLastMessageCallback);
+    When(Method(ArduinoFake(Serial), available)).Return(1, 0, 1, 0);
+    When(Method(ArduinoFake(Serial), read)).Return(':', '#');
+
+    // Execute
+    underTest->loop();
+    underTest->loop();
+
+    // Asserts
+    TEST_ASSERT_EQUAL_STRING(":#", lastMessage.c_str());
+}
+
+void test_VerifyAck() {
+    // Setup
+    Serial_* serial = ArduinoFakeMock(Serial);
+    NonBlockingSerialDriver *underTest = new NonBlockingSerialDriver(serial, recordLastMessageCallback);
+    When(Method(ArduinoFake(Serial), available)).Return(1, 0);
+    When(Method(ArduinoFake(Serial), read)).Return(0x06);
+    When(OverloadedMethod(ArduinoFake(Serial), write, size_t(uint8_t))).Return(1);
+
+    // Execute
+    underTest->loop();
+
+    // Asserts
+    Verify(OverloadedMethod(ArduinoFake(Serial), write, size_t(uint8_t)).Using('1')).Once();
+}
+
+void setUp(void) {
+    lastMessage = "";
+}
+
+
 int main() {
     UNITY_BEGIN();
 
     RUN_TEST(test_NoDataAvailable);
+    RUN_TEST(test_SimpleMessage_OneIteration);
+    RUN_TEST(test_SimpleMessage_TwoIterations);
+
+    RUN_TEST(test_VerifyAck);
 
     UNITY_END();
 
